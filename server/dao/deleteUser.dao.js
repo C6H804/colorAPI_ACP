8e74bba2aca217734dfd6fccc82b7dcf347cef93
C6H804 <130099103+C6H804@users.noreply.github.com>
2025-10-14 11:13:06 +0200

    refactor: Adjust grid column widths in modalAddColor.css and restore deletePermissions function in deleteUser.dao.js

--- server/dao/deleteUser.dao.js
+++ server/dao/deleteUser.dao.js
@@ -16,16 +16,16 @@ const deleteUser = async (userId) => {
 
 
 
-// const deletePermissions = async (userId) => {
-//     const stmt = "DELETE FROM users_permissions WHERE id_user = ?";
-//     const values = [userId];
-//     try {
-//         const [results] = await db.execute(stmt, values);
-//         return { valid: true, message: "user permissions deleted successfully", status: 200 };
-//     } catch (error) {
-//         console.error("error in deleteUser.dao.js:", error);
-//         return { valid: false, message: "Database error", status: 500 };
-//     }
-// }
+const deletePermissions = async (userId) => {
+    const stmt = "DELETE FROM users_permissions WHERE id_user = ?";
+    const values = [userId];
+    try {
+        const [results] = await db.execute(stmt, values);
+        return { valid: true, message: "user permissions deleted successfully", status: 200 };
+    } catch (error) {
+        console.error("error in deleteUser.dao.js:", error);
+        return { valid: false, message: "Database error", status: 500 };
+    }
+}
 
-module.exports = { deleteUser, /*deletePermissions */ };
+module.exports = { deleteUser, deletePermissions };
